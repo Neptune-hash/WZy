7a13cd3148eb8639f977b73857fe8ac2687c8bd2
Neptune-hash <80513773+Neptune-hash@users.noreply.github.com>
2021-05-22 23:24:59 +0800

    Update upload.rb

--- campusfloraweb-master/app/admin/upload.rb
+++ campusfloraweb-master/app/admin/upload.rb
@@ -2,9 +2,9 @@ ActiveAdmin.register Upload do
 
 active_admin_importable
   index do
-    column :about_page_content
-    column :lat
-    column :lon
+    column :Common Name
+    column :Indigenous Name
+    column :Information
     actions
   end
 
