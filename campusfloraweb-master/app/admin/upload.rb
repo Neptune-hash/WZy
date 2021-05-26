d5442c12f622af95b4c0ee61aabf1726b90afc05
Neptune-hash <80513773+Neptune-hash@users.noreply.github.com>
2021-05-26 19:58:04 +0800

    Update upload.rb

--- campusfloraweb-master/app/admin/upload.rb
+++ campusfloraweb-master/app/admin/upload.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Upload do
   permit_params :Common_Name, :Indigenous_Name, :Information 
-active_admin_importable do |model.hash|
+active_admin_importable do |model,hash|
 model.create(Common_Name:hash[:Common_Name],Indigenous_Name:hash[:Indigenous_Name],Information:hash[:Information] )
   #index do
    #column :Common_Name
